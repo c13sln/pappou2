8ea9135d306dab72af6f60db964ace9f4f3f0866
Sebastian Lundgren <lovtradet@gmail.com>
2018-03-26 15:56:59 +0200

    Added insert row and add column to the api.

--- ou2/DBAPI.py
+++ ou2/DBAPI.py
@@ -30,4 +30,61 @@ def create_db(name):
     conn.close()
 
 
+# Creates and adds new column to database.
+def add_column(db_name, table_name, col_name):
+    sqlite_file = db_name  # name of the sqlite database file
+    table_name = table_name  # name of the table to be created
+    id_column = 'my_1st_column'  # name of the PRIMARY KEY column
+    new_column1 = 'my_2nd_column'  # name of the new column
+    new_column2 = 'my_3nd_column'  # name of the new column
+    column_type = 'TEXT'  # E.g., INTEGER, TEXT, NULL, REAL, BLOB
+    default_val = 'Hello World'  # a default value for the new column rows
+
+    # Connecting to the database file
+    conn = sqlite3.connect(sqlite_file)
+    c = conn.cursor()
+
+    # A) Adding a new column without a row value
+    c.execute("ALTER TABLE {tn} ADD COLUMN '{cn}' {ct}" \
+              .format(tn=table_name, cn=new_column1, ct=column_type))
+
+    # B) Adding a new column with a default row value
+    c.execute("ALTER TABLE {tn} ADD COLUMN '{cn}' {ct} DEFAULT '{df}'" \
+              .format(tn=table_name, cn=new_column2, ct=column_type, df=default_val))
+
+    # Committing changes and closing the connection to the database file
+    conn.commit()
+    conn.close()
+
+
+def insert_row(db_name, table_name, id_col, col_name):
+    sqlite_file = db_name
+    table_name = table_name
+    id_column = id_col
+    column_name = col_name
+
+    # Connecting to the database file
+    conn = sqlite3.connect(sqlite_file)
+    c = conn.cursor()
+
+    # A) Inserts an ID with a specific value in a second column
+    try:
+        c.execute("INSERT INTO {tn} ({idf}, {cn}) VALUES (123456, 'test')". \
+                  format(tn=table_name, idf=id_column, cn=column_name))
+    except sqlite3.IntegrityError:
+        print('ERROR: ID already exists in PRIMARY KEY column {}'.format(id_column))
+
+    # B) Tries to insert an ID (if it does not exist yet)
+    # with a specific value in a second column
+    c.execute("INSERT OR IGNORE INTO {tn} ({idf}, {cn}) VALUES (123456, 'test')". \
+              format(tn=table_name, idf=id_column, cn=column_name))
+
+    # C) Updates the newly inserted or pre-existing entry
+    c.execute("UPDATE {tn} SET {cn}=('Hi World') WHERE {idf}=(123456)". \
+              format(tn=table_name, cn=column_name, idf=id_column))
+
+    conn.commit()
+    conn.close()
+
+
 print("Goodbye World!")
